b44a690ae9bdacce7e50a1f40df155eb146e8f57
Halil Eren Yazıcı <halilerenyazici@gmail.com>
2023-12-17 16:33:36 +0300

    Proje Oluşturuldu

---
+++ src/main/java/com/example/kuaforrandevu/KuaforRandevuApplication.java
@@ -0,0 +1,13 @@
+package com.example.kuaforrandevu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuaforRandevuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuaforRandevuApplication.class, args);
+	}
+
+}
