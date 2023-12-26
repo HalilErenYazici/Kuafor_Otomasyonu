39388b9946d4f602a5bf06ed499e631c6da6eaf0
berkaygrcn <102293169+berkaygrcn@users.noreply.github.com>
2023-12-26 22:20:56 +0200

    son hali web config

--- src/main/java/com/example/kuaforrandevu/config/WebConfig.java
+++ src/main/java/com/example/kuaforrandevu/config/WebConfig.java
@@ -8,9 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-public void addCorsMappings(CorsRegistry registry){
-    registry.addMapping("/api/**")
-            .allowedOrigins("http://127.0.0.1:5501/")
-            .allowedMethods("GET","POST","PUT","DELETE");
-}
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://127.0.0.1:5500/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
 }
